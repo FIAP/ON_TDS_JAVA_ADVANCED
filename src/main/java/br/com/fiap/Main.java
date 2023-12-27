fb6f39de79007b8762479c045b2cb2f01a34f0fc
Prof. Celso Furtado <celso@celso.dev.br>
2023-12-27 15:40:43 -0300

    Arquitetura DAO com métodos para inclusão, alteração e exclusão de registro

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,6 +1,8 @@
 package br.com.fiap;
 
+import br.com.fiap.dao.GameDao;
 import br.com.fiap.model.Game;
+import br.com.fiap.utils.Conexao;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,18 +13,23 @@ public class Main {
 
     public static void main(String[] args) {
         Game game1 = new Game();
-        game1.setTitulo("Mega Man 1");
-        game1.setCategoria("Plataforma");
-        game1.setDataLancamento(LocalDate.of(1987, 12, 1));
+        game1.setId(4L);
+        game1.setTitulo("Batletoads");
+        game1.setCategoria("Luta");
+        //game1.setDataLancamento(LocalDate.of(1992, 8, 1));
+        game1.setDataLancamento(LocalDate.of(1992, 6, 1));
         game1.setFinalizado(true);
-        game1.setProdutora("Capcom");
-        game1.setValor(128.0);
+        //game1.setProdutora("Tradewest");
+        game1.setProdutora("Tradewest, Rare");
+        game1.setValor(99.89);
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("games");
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = Conexao.getEntityManager();
+        GameDao gameDao = new GameDao(em);
 
         em.getTransaction().begin();
-        em.persist(game1);
+        //gameDao.salvar(game1);
+        //gameDao.atualizar(game1);
+        gameDao.remover(game1);
         em.getTransaction().commit();
         em.close();
 
---
+++ src/main/java/br/com/fiap/dao/GameDao.java
@@ -0,0 +1,27 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.model.Game;
+import jakarta.persistence.EntityManager;
+
+public class GameDao {
+
+    private EntityManager em;
+
+    public GameDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void salvar(Game game) {
+        em.persist(game);
+    }
+
+    public void atualizar(Game game) {
+        em.merge(game);
+    }
+
+    public void remover(Game game) {
+        Game gameExcluir = em.find(Game.class, game.getId());
+        em.remove(gameExcluir);
+    }
+
+}
