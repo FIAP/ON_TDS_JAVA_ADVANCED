c4f6c6972127699037d9ab3cd04137da25f61b0d
Prof. Celso Furtado <celso@celso.dev.br>
2023-12-28 08:45:39 -0300

    Testes do ciclo de vida da JPA

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -4,34 +4,32 @@ import br.com.fiap.dao.GameDao;
 import br.com.fiap.model.Game;
 import br.com.fiap.utils.Conexao;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Game game1 = new Game();
-        game1.setId(4L);
-        game1.setTitulo("Batletoads");
-        game1.setCategoria("Luta");
-        //game1.setDataLancamento(LocalDate.of(1992, 8, 1));
-        game1.setDataLancamento(LocalDate.of(1992, 6, 1));
+        game1.setTitulo("Ikari Warriors");
+        game1.setCategoria("Arcade");
+        game1.setDataLancamento(LocalDate.of(1986, 1, 1));
         game1.setFinalizado(true);
-        //game1.setProdutora("Tradewest");
-        game1.setProdutora("Tradewest, Rare");
-        game1.setValor(99.89);
+        game1.setProdutora("SNK");
+        game1.setValor(256.88);
 
         EntityManager em = Conexao.getEntityManager();
         GameDao gameDao = new GameDao(em);
 
         em.getTransaction().begin();
-        //gameDao.salvar(game1);
-        //gameDao.atualizar(game1);
-        gameDao.remover(game1);
+        gameDao.salvar(game1);
+        game1.setTitulo("Ikari Warriors SNK");
         em.getTransaction().commit();
+
         em.close();
+
+
 
     }
 
